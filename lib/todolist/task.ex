e86f453203a2d47f1f4521033da4d91ed3d0d5ae
NATHAN <ibguinathan@gmail.com>
2023-10-24 10:13:32 +0200

    PREMIER PUSH JUSQU'A l'avant derniere partie des routings

---
+++ lib/todolist/task.ex
@@ -0,0 +1,22 @@
+defmodule Todolist.Task do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "tasks" do
+    field :title, :string
+    field :description, :string
+    field :status, :string
+    belongs_to :user, Todolist.User
+
+    timestamps()
+  end
+
+
+
+  @doc false
+  def changeset(task, attrs) do
+    task
+    |> cast(attrs, [:title, :description, :status])
+    |> validate_required([:title, :description, :status])
+  end
+end
